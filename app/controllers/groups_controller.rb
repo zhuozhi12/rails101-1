50a9f13d0f2e4002918da7d3078217ddef49ac12
zhuozhi12 <feibou@126.com>
2017-03-29 22:52:09 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :create]
-
+before_action :authenticate_user! , only: [:new, :create, :edit, :destroy]
+before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -10,8 +10,7 @@ before_action :authenticate_user! , only: [:new, :create]
    end
 
    def edit
-     @group = Group.find(params[:id])
-    end
+  end
 
 def new
   @group = Group.new
@@ -29,23 +28,35 @@ def new
   end
 
   def update
-   @group = Group.find(params[:id])
+
    if @group.update(group_params)
      redirect_to groups_path, notice: "Update Success"
    else
      render :edit
    end
+
+
+ def destroy
+
+
+   @group.destroy
+   redirect_to groups_path, alert: "Group deleted"
  end
 
-  def destroy
-      @group = Group.find(params[:id])
-      @group.destroy
-      flash[:alert] = "Group deleted"
-      redirect_to groups_path
-    end
-
    private
+   def find_group_and_check_permission
+       @group = Group.find(params[:id])
 
+       if current_user != @group.user
+         redirect_to root_path, alert: "You have no permission."
+       end
+     end
+
+     def group_params
+       params.require(:group).permit(:title, :description)
+     end
+
+   end
    def group_params
      params.require(:group).permit(:title, :description)
    end
